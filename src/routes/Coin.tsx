d3a497ea6cf7b8b5493d2aa7a6e54e60c773f00a
Kyedonghwan <kdh643737@gmail.com>
2022-06-22 23:14:47 +0900

    20220622-2

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -20,7 +20,7 @@ const Header = styled.header`
 
 const Title = styled.h1`
     font-size: 48px;
-    color:${props => props.theme.accentColor};
+    color:${props => props.theme.accentColor}; 
 `;
 
 const Loader = styled.span`
