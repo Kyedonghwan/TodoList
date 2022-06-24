f9d29c868d9f5eb035b6e25d806aac1d4c6adcd3
Kyedonghwan <kdh643737@gmail.com>
2022-06-24 17:33:49 +0900

    20220624

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
+import { QueryClient } from 'react-query';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { darkTheme, lightTheme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const queryClient = new QueryClient()
-
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-            <App />
-      </QueryClientProvider>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
